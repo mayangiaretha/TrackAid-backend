20316acbfc7b16a36e920eb1b9f0bc7e24305ee5
mayangiaretha <myaretha2620@gmail.com>
2024-03-19 22:48:47 +0300

    added get all clients

--- src/features/clients/clients.controllers.js
+++ src/features/clients/clients.controllers.js
@@ -5,33 +5,41 @@ import { v4 as uuidv4 } from 'uuid';
 import dayjs from "dayjs";
 
 
-class ClientsControllers{
-  static async createAClient(req, res){
-    const {name,email,address, telephone } = req.body;
+class ClientsControllers {
+  static async createAClient(req, res) {
+    const { name, email, address, telephone } = req.body;
 
-      const newClient = await clientsModel.create({ email,
-        name,
-        address,
-        telephone,
-        createdAt: dayjs().format('YYYY-MM-DD h:mm:ss A'),
-        clientId: uuidv4(),});
+    const newClient = await clientsModel.create({
+      email,
+      name,
+      address,
+      telephone,
+      createdAt: dayjs().format('YYYY-MM-DD h:mm:ss A'),
+      clientId: uuidv4(),
+    });
 
-      return res.status(EnumHttpStatus.CREATED).json(newClient);
+    return res.status(EnumHttpStatus.CREATED).json(newClient);
 
 
   };
 
-  static async getAClient (req, res){
+  static async getAClient(req, res) {
     const { id } = req.params;
-      const client = await clientsModel.findOne({clientId: id});
-      if (!client)
-        return res.status(EnumHttpStatus.NOT_FOUND).json({ message: 'Client not found' });
-        return res.status(EnumHttpStatus.OK).json(client);
+    const client = await clientsModel.findOne({ clientId: id });
+    if (!client)
+      return res.status(EnumHttpStatus.NOT_FOUND).json({ message: 'Client not found' });
+    return res.status(EnumHttpStatus.OK).json(client);
 
   };
 
-}
+  static async getAllClients(req, res) {
 
+    const clients = await clientsModel.find();
+    return res.status(EnumHttpStatus.OK).json(clients);
+
+
+  };
+}
 
 
 export default ClientsControllers
--- src/features/clients/clients.route.js
+++ src/features/clients/clients.route.js
@@ -6,6 +6,7 @@ const router = Router();
 
 router.post('/', asyncMiddleware(clientsControllers.createAClient));
 router.get('/:id', asyncMiddleware(clientsControllers.getAClient));
+router.get('/', asyncMiddleware(clientsControllers.getAllClients));
 
 export default router;
 
